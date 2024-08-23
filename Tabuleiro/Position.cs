077f06059b75312a6694909a45c9d57ea9bb9148
Patrick Oliveira <joaopatrick65@gmail.com>
2024-08-23 16:05:21 -0300

    Criada classe Position

---
+++ Tabuleiro/Position.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Tabuleiro
+{
+    internal class Position
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Position()
+        {
+
+        }
+
+        public Position(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return Linha + ", " + Coluna;
+        }
+    }
+}
