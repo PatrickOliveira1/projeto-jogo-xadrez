403823d31940f99cf4e5b6cb2ded354091031580
Patrick Oliveira <joaopatrick65@gmail.com>
2024-08-26 16:35:37 -0300

    Criando a classe PositionChess

---
+++ Xadrez/PositionChess.cs
@@ -0,0 +1,31 @@
+﻿using Tabuleiro;
+
+namespace Xadrez
+{
+    internal class PositionChess
+    {
+        public char Column { get; set; }
+        public int Row { get; set; }
+
+        public PositionChess()
+        {
+
+        }
+
+        public PositionChess(char column, int row)
+        {
+            Column = column;
+            Row = row;
+        }
+
+        public Position toPosition()
+        {
+            return new Position(8 - Row, Column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Column + Row;
+        }
+    }
+}
